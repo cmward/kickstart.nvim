ea80689a209b6ed9b141551643bdc1052a740a0d
Chris Ward <chrisward.cmw@gmail.com>
2024-08-26 22:12:00 -0400

    Initial setup for rust dev

--- init.lua
+++ init.lua
@@ -233,7 +233,6 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -657,6 +656,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'joker',
+        'rustfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -772,7 +772,59 @@ require('lazy').setup({
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+          autocomplete = false,
+        },
+
+        formatting = {
+          -- https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678
+          fields = { 'abbr', 'menu', 'kind' },
+          format = function(entry, item)
+            -- Define menu shorthand for different completion sources.
+            local menu_icon = {
+              nvim_lsp = 'NLSP',
+              nvim_lua = 'NLUA',
+              luasnip = 'LSNP',
+              buffer = 'BUFF',
+              path = 'PATH',
+            }
+            -- Set the menu "icon" to the shorthand for each completion source.
+            item.menu = menu_icon[entry.source.name]
+
+            -- Set the fixed width of the completion menu.
+            local fixed_width = 30
+
+            -- Set 'fixed_width' to false if not provided.
+            fixed_width = fixed_width or false
+
+            -- Get the completion entry text shown in the completion window.
+            local content = item.abbr
+
+            -- Set the fixed completion window width.
+            if fixed_width then
+              vim.o.pumwidth = fixed_width
+            end
+
+            -- Get the width of the current window.
+            local win_width = vim.api.nvim_win_get_width(0)
+
+            -- Set the max content width based on either: 'fixed_width'
+            -- or a percentage of the window width, in this case 20%.
+            -- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+            local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+            -- Truncate the completion entry text if it's longer than the
+            -- max content width. We subtract 3 from the max content width
+            -- to account for the "..." that will be appended to it.
+            if #content > max_content_width then
+              item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. '...'
+            else
+              item.abbr = content .. (' '):rep(max_content_width - #content)
+            end
+            return item
+          end,
+        },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
@@ -791,11 +843,11 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
           --['<Tab>'] = cmp.mapping.select_next_item(),
           --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 

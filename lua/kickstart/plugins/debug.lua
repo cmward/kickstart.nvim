8c97191da36ef9c9f0b54261ddb57a3dda54c202
cmward <chrisward.cmw@gmail.com>
2026-07-27 20:24:04 -0400

    gd dap

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -41,7 +41,6 @@ require('mason-nvim-dap').setup {
   -- online, please don't ask me how to install them :)
   ensure_installed = {
     -- Update this to ensure that you have the debuggers for the langs you want
-    'delve',
     'codelldb'
   },
 }
@@ -125,3 +124,19 @@ local cpp_config = {
 dap.configurations.cpp = cpp_config
 dap.configurations.c = cpp_config
 dap.configurations.zig = cpp_config
+
+dap.adapters.godot = {
+  type = 'server',
+  host = '127.0.0.1',
+  port = 6006, -- Godot's default DAP port
+}
+
+dap.configurations.gdscript = {
+  {
+    type = 'godot',
+    request = 'launch',
+    name = 'Launch scene',
+    project = '${workspaceFolder}',
+    launch_scene = true,
+  },
+}
